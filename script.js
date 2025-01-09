0c6d0c40203421780ee4b50b6489133515225247
yswef alhmzy <astamylove58@gmail.com>
2025-01-09 17:16:07 +0300

    Build a personal portfolio website from scratch, including an About Me section and a Projects showcase section, to highlight skills and completed work. Prepared the project for deployment and uploaded it to GitHub for version control and sharing.

---
+++ script.js
@@ -0,0 +1,40 @@
+const themeToggle = document.getElementById('toggle-theme');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark-theme');
+    themeToggle.textContent = 'Light Mode';
+}
+
+themeToggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    const theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+    this.textContent = theme === 'dark' ? 'Light Mode' : 'Dark Mode';
+});
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    
+});
+// إيقاف التمرير عند الضغط بالماوس
+swiper.el.addEventListener('mouseenter', function() {
+    swiper.autoplay.stop(); // إيقاف التمرير التلقائي عند الماوس فوق العنصر
+});
+
+swiper.el.addEventListener('mouseleave', function() {
+    swiper.autoplay.start(); // إعادة تشغيل التمرير التلقائي عند مغادرة الماوس
+});
